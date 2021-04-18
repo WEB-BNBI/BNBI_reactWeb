c6f96077623e3d3c8aa1f1f2c547ab621f426e07
DESKTOP-8U7E8U4\allen <h88129@gmail.com>
2021-04-18 23:30:53 +0800

    fetch api part

--- src/component/utility/table/table.js
+++ src/component/utility/table/table.js
@@ -1,98 +1,58 @@
-import React from 'react';
+import React , { useState } from 'react';
 import 'antd/dist/antd.css';
 import './table.css';
-import { Table } from 'antd';
-import reqwest from 'reqwest';
-
+import { Table, Tag, Space } from 'antd';
 const columns = [
   {
     title: 'Name',
     dataIndex: 'name',
-    sorter: true,
-    render: name => `${name.first} ${name.last}`,
-    width: '20%'
+    key: 'name',
+    render: text => <a>{text}</a>,
   },
   {
-    title: 'Gender',
-    dataIndex: 'gender',
-    width: '20%',
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
   },
   {
-    title: 'Email',
-    dataIndex: 'email',
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    dataIndex: 'action'
   },
 ];
 
-const getRandomuserParams = params => ({
-  results: params.pagination.pageSize,
-  page: params.pagination.current,
-  ...params,
-});
-
-class App extends React.Component {
-  constructor(props) { // 加入建構子以及props參數
-    super(props);
+const App = (props) => {
+  const fetchData = async (param)=>{
+    try{
+        const data = await fetch('http://127.0.0.1:3001/dev/api/v1/law/getVerdictList/八十五年度再審字第六一八號',{
+            method:"get",
+            body: param // encode 
+        }).then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(myJson);
+          return (myJson)
+        });
+        return(data);
+    }catch(e){
+        console.error(e)
+    }
 }
-  state = {
-    data: [],
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-    loading: false,
-  };
-
-  componentDidMount() {
-    const { pagination } = this.state;
-    this.fetch({ pagination });
-  }
-
-  handleTableChange = (pagination, filters, sorter) => {
-    this.fetch({
-      sortField: sorter.field,
-      sortOrder: sorter.order,
-      pagination,
-      ...filters,
-    });
-  };
-
-  fetch = (params = {}) => {
-    this.setState({ loading: true });
-    reqwest({
-      url: 'https://randomuser.me/api',
-      method: 'get',
-      type: 'json',
-      data: getRandomuserParams(params),
-    }).then(data => {
-      console.log(data);
-      this.setState({
-        loading: false,
-        data: data.results,
-        pagination: {
-          ...params.pagination,
-          total: 200,
-          // 200 is mock data, you should read it from server
-          // total: data.totalCount,
-        },
-      });
-    });
-  };
-
-  render() {
-    const { data, pagination, loading } = this.state;
-    const { className } = this.props;
-    return (
-      <Table
-        columns={columns}
-        rowKey={record => record.login.uuid}
-        dataSource={data}
-        pagination={pagination}
-        loading={loading}
-        onChange={this.handleTableChange}
-        className={this.props.className}
-      />
-    );
-  }
+const data2=fetchData();
+  return(
+    <Table columns={columns} dataSource={data2} />
+  )
 }
 
 export default App;
